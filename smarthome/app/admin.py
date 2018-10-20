c9884f9f4a9762e26e81e42530a306642896f760
KamilKwapisz <kamiltm1998@gmail.com>
2018-10-20 14:50:00 +0200

    user profiles are now created automatically

--- smarthome/app/admin.py
+++ smarthome/app/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
-from .models import Profile, Host, Tenant
+from .models import Profile, Host, Tenant, Room, Rent
 
 admin.site.register(Profile)
 admin.site.register(Host)
 admin.site.register(Tenant)
+admin.site.register(Room)
+admin.site.register(Rent)
